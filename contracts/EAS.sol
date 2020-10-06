2496388ba796c7d7d9b7e8a244c098a901951d92
lbeder <lbeder@users.noreply.github.com>
2020-10-06 16:50:41 +0300

    Rename AIO to AO

--- contracts/EAS.sol
+++ contracts/EAS.sol
@@ -12,7 +12,7 @@ contract EAS {
     // A data struct representing a single attestation.
     struct Attestation {
         bytes32 uuid;
-        uint32 aio;
+        uint32 ao;
         address from;
         uint256 time;
         uint256 expirationTime;
@@ -20,14 +20,14 @@ contract EAS {
         string data;
     }
 
-    // A data struct representing attestations to a specific AIO (by their UIIDs).
-    struct AttestationIdentityObject {
-        // A list of attestations IDs, belonging to this AIO.
+    // A data struct representing attestations to a specific AO (by their UIIDs).
+    struct AttestationObject {
+        // A list of attestations IDs, belonging to this AO.
         bytes32[] attestationUIIDs;
     }
 
-    // A mapping between an account, its AIOs and their respective attestations.
-    mapping (address => mapping (uint32 => AttestationIdentityObject)) private attestations;
+    // A mapping between an account, its AOs and their respective attestations.
+    mapping (address => mapping (uint32 => AttestationObject)) private attestations;
 
     // A global mapping between attestations and their UUIDs.
     mapping (bytes32 => Attestation) private db;
@@ -38,32 +38,32 @@ contract EAS {
     /// @dev Triggered when an attestation has been made.
     ///
     /// @param _attester The attesting account.
-    /// @param _aio The ID of the AIO.
+    /// @param _ao The ID of the AO.
     /// @param _uuid The UUID the revoked attestation.
-    event Attested(address indexed _attester, uint32 indexed _aio, bytes32 indexed _uuid);
+    event Attested(address indexed _attester, uint32 indexed _ao, bytes32 indexed _uuid);
 
     /// @dev Triggered when an attestation has been revoked.
     ///
     /// @param _attester The attesting account.
-    /// @param _aio The ID of the AIO.
+    /// @param _ao The ID of the AO.
     /// @param _uuid The UUID the revoked attestation.
-    event Revoked(address indexed _attester, uint32 indexed _aio, bytes32 indexed _uuid);
+    event Revoked(address indexed _attester, uint32 indexed _ao, bytes32 indexed _uuid);
 
-    /// @dev Attests to a specific AIO.
+    /// @dev Attests to a specific AO.
     ///
     /// @param _recipient The recipient the attestation.
-    /// @param _aio The ID of the AIO.
+    /// @param _ao The ID of the AO.
     /// @param _expirationTime The expiration time of the attestation.
     /// @param _data The additional attestation data.
-    function attest(address _recipient, uint32 _aio, uint256 _expirationTime, string calldata _data) public {
-        AttestationIdentityObject storage aio = attestations[_recipient][_aio];
+    function attest(address _recipient, uint32 _ao, uint256 _expirationTime, string calldata _data) public {
+        AttestationObject storage ao = attestations[_recipient][_ao];
 
         require(_recipient != msg.sender, "ERR_INVALID_ATTESTER");
         require(_expirationTime > block.timestamp, "ERR_INVALID_EXPIRATION_TIME");
 
         Attestation memory attestation = Attestation({
             uuid: EMPTY_UUID,
-            aio: _aio,
+            ao: _ao,
             from: msg.sender,
             time: block.timestamp,
             expirationTime: _expirationTime,
@@ -74,15 +74,15 @@ contract EAS {
         bytes32 uuid = getUUID(attestation);
         attestation.uuid = uuid;
 
-        aio.attestationUIIDs.push(uuid);
+        ao.attestationUIIDs.push(uuid);
 
         db[uuid] = attestation;
         attestationsCount++;
 
-        emit Attested(msg.sender, _aio, uuid);
+        emit Attested(msg.sender, _ao, uuid);
     }
 
-    /// @dev Revokes an existing attestation to a specific AIO.
+    /// @dev Revokes an existing attestation to a specific AO.
     ///
     /// @param _uuid The UUID of the attestation to revoke.
     function revoke(bytes32 _uuid) public {
@@ -91,7 +91,7 @@ contract EAS {
 
         attestation.revocationTime = block.timestamp;
 
-        emit Revoked(msg.sender, attestation.aio, EMPTY_UUID);
+        emit Revoked(msg.sender, attestation.ao, EMPTY_UUID);
     }
 
     /// @dev Returns an existing attestation by UUID.
@@ -103,7 +103,7 @@ contract EAS {
         Attestation memory attestation = db[_uuid];
 
         return (
-            attestation.aio,
+            attestation.ao,
             attestation.from,
             attestation.time,
             attestation.expirationTime,
@@ -117,7 +117,7 @@ contract EAS {
     /// @param _attestation The input attestation.
     function getUUID(Attestation memory _attestation) private view returns (bytes32) {
         return keccak256(abi.encodePacked(
-            _attestation.aio, HASH_SEPARATOR,
+            _attestation.ao, HASH_SEPARATOR,
             _attestation.from, HASH_SEPARATOR,
             _attestation.time, HASH_SEPARATOR,
             _attestation.expirationTime, HASH_SEPARATOR,
