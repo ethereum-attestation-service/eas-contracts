9b05c7979e1c0bbd1f3d823596f43d996edbeec1
lbeder <lbeder@users.noreply.github.com>
2020-10-10 22:47:44 +0300

    Add sample attestation verifier tests

---
+++ contracts/tests/verifiers/TestAOAttestationVerifier.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "../../IAOVerifier.sol";
+import "../../EAS.sol";
+
+/// @title A sample AO verifier that checks whether an attestations attest to an existing attestation.
+contract TestAOAttestationVerifier is IAOVerifier {
+    EAS public eas;
+
+    constructor(EAS _eas) public {
+        eas = _eas;
+    }
+
+    function verify(
+        address, /* _recipient */
+        bytes calldata, /* _schema */
+        bytes calldata _data,
+        uint256, /* _expirationTime */
+        address, /* _msgSender */
+        uint256 /* _msgValue */
+    ) public virtual override view returns (bool) {
+        (bytes32 uuid, , , , , , , ) = eas.getAttestation(toBytes32(_data, 0));
+
+        return uuid != bytes32(0x0);
+    }
+
+    function toBytes32(bytes memory _bytes, uint256 _start) private pure returns (bytes32) {
+        require(_start + 32 >= _start, "ERR_OVERFLOW");
+        require(_bytes.length >= _start + 32, "ERR_OUT_OF_BOUNDS");
+        bytes32 tempBytes32;
+
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            tempBytes32 := mload(add(add(_bytes, 0x20), _start))
+        }
+
+        return tempBytes32;
+    }
+}
