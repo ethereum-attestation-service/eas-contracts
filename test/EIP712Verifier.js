edb22449ca2db2eeeb35623bd2adc6268b2b3c4d
Leonid Beder <leonid.beder@gmail.com>
2020-10-22 18:12:52 +0300

    Refactor using @ethereum-attestation-service/sdk

--- test/EIP712Verifier.js
+++ test/EIP712Verifier.js
@@ -1,4 +1,8 @@
 const { expect } = require('chai');
+const { utils } = require('ethers');
+const { Proxy, ATTEST_TYPE, REVOKE_TYPE } = require('@ethereum-attestation-service/sdk');
+
+const { keccak256, toUtf8Bytes } = utils;
 
 const EIP712Verifier = require('./helpers/eip712Verifier');
 
@@ -10,14 +14,19 @@ contract('EIP712Verifier', () => {
   });
 
   it('should return the correct domain separator', async () => {
-    expect(await EIP712Verifier.getDomainSeparator(verifier)).to.eql(await verifier.getDomainSeparator());
+    const proxy = new Proxy({
+      address: verifier.getAddress(),
+      version: await verifier.getVersion(),
+      chainId: 1
+    });
+    expect(await verifier.getDomainSeparator()).to.eql(proxy.getDomainSeparator());
   });
 
   it('should return the attest type hash', async () => {
-    expect(EIP712Verifier.getAttestTypeHash()).to.eql(await verifier.getAttestTypeHash());
+    expect(await verifier.getAttestTypeHash()).to.eql(keccak256(toUtf8Bytes(ATTEST_TYPE)));
   });
 
   it('should return the revoke type hash', async () => {
-    expect(EIP712Verifier.getRevokeTypeHash()).to.eql(await verifier.getRevokeTypeHash());
+    expect(await verifier.getRevokeTypeHash()).to.eql(keccak256(toUtf8Bytes(REVOKE_TYPE)));
   });
 });
