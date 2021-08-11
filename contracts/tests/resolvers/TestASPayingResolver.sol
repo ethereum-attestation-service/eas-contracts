a38ea9e0f4b992c2b7e71bb861ebadeafcf5750a
Leonid Beder <leonid.beder@gmail.com>
2021-08-11 23:10:53 +0100

    Add TestASPayingResolver

---
+++ contracts/tests/resolvers/TestASPayingResolver.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+import "../../ASResolver.sol";
+
+/**
+ * @title A sample AS resolver that pays attesters
+ */
+contract TestASPayingResolver is ASResolver {
+    uint256 private immutable _incentive;
+
+    constructor(uint256 incentive) {
+        _incentive = incentive;
+    }
+
+    function isPayable() public pure override returns (bool) {
+        return true;
+    }
+
+    function resolve(
+        address recipient,
+        bytes calldata, /* schema */
+        bytes calldata, /* data */
+        uint256, /* expirationTime */
+        address /* msgSender */
+    ) external payable virtual override returns (bool) {
+        payable(recipient).transfer(_incentive);
+
+        return true;
+    }
+}
