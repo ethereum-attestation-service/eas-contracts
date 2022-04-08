577b237b036fdffeb55a98aeadfc0d54f7a475f1
lbeder <lbeder@users.noreply.github.com>
2022-04-08 17:35:31 +0100

    Upgrade Solidity to 0.8.13

--- contracts/tests/resolvers/TestASTokenResolver.sol
+++ contracts/tests/resolvers/TestASTokenResolver.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.11;
+pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
