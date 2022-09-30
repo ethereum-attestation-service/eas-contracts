695d5cf034b873d74c49d8cdfbe107d163e8c5c0
Leonid Beder <>
2022-09-30 13:33:25 +0100

    Style

--- contracts/EIP712Verifier.sol
+++ contracts/EIP712Verifier.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
 import { IEIP712Verifier } from "./IEIP712Verifier.sol";
 
