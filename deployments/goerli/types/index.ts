2fb093e58c26b88c7b4d860eb17248901f3c0854
lbeder <lbeder@users.noreply.github.com>
2022-10-05 15:04:05 +0100

    Fix exports

---
+++ deployments/goerli/types/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+
+/* eslint-disable */
+
+export type { EAS } from './contracts/EAS';
+export { EAS__factory } from './factories/contracts/EAS__factory';
+export type { EIP712Verifier } from './contracts/EIP712Verifier';
+export { EIP712Verifier__factory } from './factories/contracts/EIP712Verifier__factory';
+export type { SchemaRegistry } from './contracts/SchemaRegistry';
+export { SchemaRegistry__factory } from './factories/contracts/SchemaRegistry__factory';
