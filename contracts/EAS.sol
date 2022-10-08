8c30e572a1dca8a758c4c9cabc36c82af8bd1967
Leonid Beder <>
2022-10-08 18:21:36 +0100

    Bump version

--- contracts/EAS.sol
+++ contracts/EAS.sol
@@ -25,7 +25,7 @@ contract EAS is IEAS {
     error NotPayable();
 
     // The version of the contract.
-    string public constant VERSION = "0.11";
+    string public constant VERSION = "0.12";
 
     // The global schema registry.
     ISchemaRegistry private immutable _schemaRegistry;
