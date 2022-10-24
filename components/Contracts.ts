522e8c196286c285aa8982f8c851884b38c5cfb8
lbeder <lbeder@users.noreply.github.com>
2022-10-24 20:56:15 +0100

    Refactor

--- components/Contracts.ts
+++ components/Contracts.ts
@@ -17,6 +17,7 @@ import {
 } from '../typechain-types';
 import { ContractFactory, Signer } from 'ethers';
 import { ethers } from 'hardhat';
+import { ABI } from 'hardhat-deploy/types';
 
 export * from '../typechain-types';
 
@@ -29,46 +30,72 @@ type AsyncReturnType<T extends (...args: any) => any> = T extends (...args: any)
 type Contract<F extends ContractFactory> = AsyncReturnType<F['deploy']>;
 
 export interface ContractBuilder<F extends ContractFactory> {
+  metadata: {
+    contractName: string;
+    abi: ABI;
+    bytecode: string;
+  };
   deploy(...args: Parameters<F['deploy']>): Promise<Contract<F>>;
   attach(address: string, passedSigner?: Signer): Promise<Contract<F>>;
 }
 
-const deployOrAttach = <F extends ContractFactory>(contractName: string): ContractBuilder<F> => {
+export type FactoryConstructor<F extends ContractFactory> = {
+  new (signer?: Signer): F;
+  abi: unknown;
+  bytecode: string;
+};
+
+export const deployOrAttach = <F extends ContractFactory>(
+  contractName: string,
+  FactoryConstructor: FactoryConstructor<F>,
+  initialSigner?: Signer
+): ContractBuilder<F> => {
   return {
-    deploy: async (...args: Parameters<F['deploy']>): Promise<Contract<F>> => {
-      const defaultSigner = (await ethers.getSigners())[0];
-      return (await ethers.getContractFactory(contractName, defaultSigner)).deploy(...args) as Contract<F>;
+    metadata: {
+      contractName,
+      abi: FactoryConstructor.abi as ABI,
+      bytecode: FactoryConstructor.bytecode
     },
-    attach: attachOnly<F>(contractName).attach
+    deploy: async (...args: Parameters<F['deploy']>): Promise<Contract<F>> => {
+      const defaultSigner = initialSigner ?? (await ethers.getSigners())[0];
+
+      return new FactoryConstructor(defaultSigner).deploy(...(args || [])) as Promise<Contract<F>>;
+    },
+    attach: attachOnly<F>(FactoryConstructor, initialSigner).attach
   };
 };
 
-const attachOnly = <F extends ContractFactory>(contractName: string) => {
+export const attachOnly = <F extends ContractFactory>(
+  FactoryConstructor: FactoryConstructor<F>,
+  initialSigner?: Signer
+) => {
   return {
     attach: async (address: string, signer?: Signer): Promise<Contract<F>> => {
-      const defaultSigner = (await ethers.getSigners())[0];
-      return ethers.getContractAt(contractName, address, signer ?? defaultSigner) as Contract<F>;
+      const defaultSigner = initialSigner ?? (await ethers.getSigners())[0];
+      return new FactoryConstructor(signer ?? defaultSigner).attach(address) as Contract<F>;
     }
   };
 };
 
 /* eslint-disable camelcase */
-const getContracts = () => ({
-  AttestationResolver: deployOrAttach<AttestationResolver__factory>('AttestationResolver'),
-  AttesterResolver: deployOrAttach<AttesterResolver__factory>('AttesterResolver'),
-  DataResolver: deployOrAttach<DataResolver__factory>('DataResolver'),
-  EAS: deployOrAttach<EAS__factory>('EAS'),
-  EIP712Verifier: deployOrAttach<EIP712Verifier__factory>('EIP712Verifier'),
-  ExpirationTimeResolver: deployOrAttach<ExpirationTimeResolver__factory>('ExpirationTimeResolver'),
-  PayingResolver: deployOrAttach<PayingResolver__factory>('PayingResolver'),
-  RecipientResolver: deployOrAttach<RecipientResolver__factory>('RecipientResolver'),
-  RevocationResolver: deployOrAttach<RevocationResolver__factory>('RevocationResolver'),
-  SchemaRegistry: deployOrAttach<SchemaRegistry__factory>('SchemaRegistry'),
-  TestEAS: deployOrAttach<TestEAS__factory>('TestEAS'),
-  TestERC20Token: deployOrAttach<TestERC20Token__factory>('TestERC20Token'),
-  TestSchemaResolver: deployOrAttach<TestSchemaResolver__factory>('TestSchemaResolver'),
-  TokenResolver: deployOrAttach<TokenResolver__factory>('TokenResolver'),
-  ValueResolver: deployOrAttach<ValueResolver__factory>('ValueResolver')
+const getContracts = (signer?: Signer) => ({
+  connect: (signer: Signer) => getContracts(signer),
+
+  AttestationResolver: deployOrAttach('AttestationResolver', AttestationResolver__factory, signer),
+  AttesterResolver: deployOrAttach('AttesterResolver', AttesterResolver__factory, signer),
+  DataResolver: deployOrAttach('DataResolver', DataResolver__factory, signer),
+  EAS: deployOrAttach('EAS', EAS__factory, signer),
+  EIP712Verifier: deployOrAttach('EIP712Verifier', EIP712Verifier__factory, signer),
+  ExpirationTimeResolver: deployOrAttach('ExpirationTimeResolver', ExpirationTimeResolver__factory, signer),
+  PayingResolver: deployOrAttach('PayingResolver', PayingResolver__factory, signer),
+  RecipientResolver: deployOrAttach('RecipientResolver', RecipientResolver__factory, signer),
+  RevocationResolver: deployOrAttach('RevocationResolver', RevocationResolver__factory, signer),
+  SchemaRegistry: deployOrAttach('SchemaRegistry', SchemaRegistry__factory, signer),
+  TestEAS: deployOrAttach('TestEAS', TestEAS__factory, signer),
+  TestERC20Token: deployOrAttach('TestERC20Token', TestERC20Token__factory, signer),
+  TestSchemaResolver: deployOrAttach('TestSchemaResolver', TestSchemaResolver__factory, signer),
+  TokenResolver: deployOrAttach('TokenResolver', TokenResolver__factory, signer),
+  ValueResolver: deployOrAttach('ValueResolver', ValueResolver__factory, signer)
 });
 /* eslint-enable camelcase */
 
