9b12be2cece45d25ee5a8a9abc48e11f88a54b42
Leonid Beder <>
2022-11-17 18:17:26 +0000

    Refactor prettier

--- contracts/tests/TestERC20Token.sol
+++ contracts/tests/TestERC20Token.sol
@@ -5,11 +5,7 @@ pragma solidity 0.8.17;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20Token is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 totalSupply
-    ) ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol, uint256 totalSupply) ERC20(name, symbol) {
         _mint(msg.sender, totalSupply);
     }
 }
