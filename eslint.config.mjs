33b857e6ffd068a159bad59087fc530d591ca39b
lbeder <leonid@lbeder.com>
2024-08-08 17:49:52 +0100

    Update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,6 @@ export default [
       ],
       semi: ['error', 'always'],
       quotes: ['error', 'single', { avoidEscape: true }],
-      'constructor-super': 'error',
       'no-plusplus': 'off',
       'no-await-in-loop': 'off',
       'no-restricted-syntax': 'off',
